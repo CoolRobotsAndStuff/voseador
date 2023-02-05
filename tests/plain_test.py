577ccec20a28c0cc3df11b0688406f6fbc000a68
ale <aledeum.saf@gmail.com>
2023-02-05 17:38:43 -0300

    changed test

--- tests/plain_test.py
+++ tests/plain_test.py
@@ -2,6 +2,6 @@ from voseador import Voseador
 
 voseador = Voseador()
 
-vos_verb = voseador.vos_from_vosotros(mood="condicional", tense="presente", infinitivo="comer", vosotros_verb="coméis")
+vos_verb = voseador.vos_from_vosotros(mood="indicativo", tense="presente", infinitivo="comer", vosotros_verb="coméis")
 
 print(vos_verb)
